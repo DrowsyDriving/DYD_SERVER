115a834d252ac4eefb009c3b44bc4bdf4cb952a0
Sepro <shtmdwns0@gmail.com>
2024-02-10 20:28:45 +0900

    마지막 설정 (아마도)

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ app.register_blueprint(api_login.bp)
 app.register_blueprint(api_alert.bp)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
 
 # app.register_blueprint(login.bp)
 # app.register_blueprint(alert.bp)
