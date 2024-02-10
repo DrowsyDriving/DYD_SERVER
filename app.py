96b1cfc846d16e07bcf6c6b6ea42e32a6b898e68
Sepro <shtmdwns0@gmail.com>
2024-02-10 19:27:23 +0900

    전체적으로 코드 수정

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from service import login, alert
-
-app = Flask(__name__)
-
-app.config['JSON_AS_ASCII'] = False
-app.register_blueprint(login.bp)
-app.register_blueprint(alert.bp)
