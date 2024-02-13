347cfe18a9a9c1acac8a8952b236f095abdcaad9
Sepro <shtmdwns0@gmail.com>
2024-02-14 00:43:14 +0900

    수정

--- service/api_login.py
+++ service/api_login.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, session
+from flask import Blueprint, request
 from database.model import AreaInfo
 bp = Blueprint("login", __name__, url_prefix='/users')
 
@@ -9,13 +9,11 @@ def login():
     result = AreaInfo.query.filter_by(area=data['area'], areacode=data['areacode']).first()
     if not result:
         return {"message": "로그인에 실패했습니다.", "success": False}, 401
-    session['area'] = data['area']
     return {"message": "로그인에 성공했습니다.", "success": True}, 200
 
 
 @bp.route('/logout', methods=['GET'])
 def logout():
     if request.method == "GET":
-        session.pop('area')
         return {"message": "로그아웃에 성공했습니다."}, 200
     return {"message": "로그아웃에 실패했습니다."}, 400
